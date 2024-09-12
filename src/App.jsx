0a33a940ec77a06d34ff667ee6848ef028732236
ali89akbar <alia42268@gmail.com>
2024-09-12 15:47:38 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,38 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import styles from './style'
+import {Navbar,Hero,Billing,Business,Clients,CTA,Feedback,Getstarted,Footer,Stats,Testominal} from './components/index';
+
+
+
+
 function App() {
 
   return (
   <div className='bg-green-300 w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar/>
 
       </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-      Hero
+      <Hero/>
       </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-      Stats 
-      Business
-      Billing
-      CardDeal
-      Testimonials
-
-      Clients
-      CTA
-      Footers
+      <Stats/> 
+      <Business/>
+      <Billing/>
+      <CardDeal/>
+      <Testominal/>
+      <Clients/>
+      <CTA/>
+      <Footer/>
       </div>
     </div>
   </div>
---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Billing from './Billing'
+import Business from './Business'
+import Button from './Button'
+import CTA from './CTA'
+import Carddeal from './Carddeal'
+import Feedback from './Feedback'
+import Footer from './Footer'
+import Hero from './Hero'
+import Getstarted from './Getstarted'
+import Clients from './Clients'
+import Testominal from './Testominal'
+import Stats from './Stats'
+export {
+    Billing,
+    Business,
+    Button,
+    CTA,
+    Carddeal,
+    Feedback,
+    Footer,
+    Hero,
+    Getstarted,
+    Clients,
+    Testominal,
+    Stats,
+    // Add any other components you want to export here. For example, ProductList, ProductDetails, etc.
+}
