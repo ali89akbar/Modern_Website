e96312ce9e994bffe7d9aa2604db1272e570281e
ali89akbar <alia42268@gmail.com>
2024-09-14 19:34:45 +0500

    main

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -6,7 +6,10 @@ const Billing = () => (
   <section id='product' className={layout.sectionImgReverse}>
     <div className={layout.sectionImgReverse}>
       <img src={bill} alt="" className={`size-100% relative z-[5]`}/>
-      <div className='a'></div>
+      <div className='absolute z-[3] -left-1/2 top-0 size-[50%] rounded-full white__gradient'/>
+      
+      <div className='absolute z-[0] -left-1/2 bottom-0 size-[50%] rounded-full pink__gradient'/>
+      
     </div>
   </section>
 )
