1ce50369abbf54b12359abb4dbe2f48d1c15f198
ali89akbar <alia42268@gmail.com>
2024-09-14 19:41:53 +0500

    main

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -3,14 +3,24 @@ import { apple, bill, google } from '../assets'
 import styles, { layout } from '../style'
 
 const Billing = () => (
-  <section id='product' className={layout.sectionImgReverse}>
+  <section id='product' className={layout.sectionReverse}>
     <div className={layout.sectionImgReverse}>
       <img src={bill} alt="" className={`size-100% relative z-[5]`}/>
       <div className='absolute z-[3] -left-1/2 top-0 size-[50%] rounded-full white__gradient'/>
       
       <div className='absolute z-[0] -left-1/2 bottom-0 size-[50%] rounded-full pink__gradient'/>
-      
-    </div>
+      </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden'/>
+      billing & invoicing</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, ducimus quod at cupiditate ex sit ut maxime, cumque rem, voluptate excepturi repellendus. Itaque quam recusandae ipsam accusamus voluptates necessitatibus voluptate!
+
+      </p>
+      </div>  
+   
+
   </section>
 )
 
