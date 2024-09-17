be2facba960a2b2be0ff53b77ad0a21425174c21
ali89akbar <alia42268@gmail.com>
2024-09-17 23:17:47 +0500

    main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,13 @@ const Footer = () => (
       <img src={logo} alt=""  className='w-[266px] h-[72px] object-contain'/>
     <p className={`${styles.paragraph} mt-4 max-w-[310px] `}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, voluptatum.</p>
     </div>
-    <div className={`flex-1.5 w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10`}>
+    <div className={`flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10`}>
       {footerLinks.map((footerLinks)=>(
         <div key={footerLinks.key} className='flex flex-col ss:my-0 my-4 min-w-[150px]' >
           <h4 className={`font-poppins font-medium text-[18px] leading-[27px] text-white`}>{footerLinks.title}</h4>
         <ul className='list-none mt-4'>
           {footerLinks.links.map((link,index)=>(
-            <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary  cursor-pointer ${index !== footerLinks.length - 1? 'mb-4' : 'mb-0' }`}>
+            <li key={index} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary  cursor-pointer ${index !== footerLinks.length - 1? 'mb-4' : 'mb-0' }`}>
             {link.name }
             </li>
           ))}
