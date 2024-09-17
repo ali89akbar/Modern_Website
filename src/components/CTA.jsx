fb20925f268a8b9542a8c3c3809d5b32ad4d7d8e
ali89akbar <alia42268@gmail.com>
2024-09-17 22:55:41 +0500

    main

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () =>(
   <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow` }>
     <div className='flex-1 flex flex-col'>
       <h2 className={`${styles.heading2}`}>Let's try our services now</h2>
-      <p className={`${styles.paragraph} mac-w-[470px] mt-5`}>Everthing you need to access card payments and grow your business</p>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Everthing you need to access card payments and grow your business</p>
     </div>
     <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10` }><Button/></div>
   </section>
